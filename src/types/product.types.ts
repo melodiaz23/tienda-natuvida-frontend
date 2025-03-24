4c33589be1f91d2e57ad8198e06a682753adb550
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-03-23 23:06:20 -0500

    Add TypeScript interfaces for API responses, product forms, orders, and shop types

---
+++ src/types/product.types.ts
@@ -0,0 +1,55 @@
+import { OrderItem } from "./orderItem.types";
+
+export interface ProductPricing {
+  id: string | undefined;
+  unitPrice: number;
+  priceTwoUnits?: number;
+  priceThreeUnits?: number;
+  previousPrice?: number;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  preparation: string;
+  ingredients: string;
+  pricing: ProductPricing;
+  category: Category | null;
+  images: ProductImage[];
+  createdAt: string;
+  updatedAt: string;
+  orderItems: OrderItem[];
+  primaryImageUrl: string | null;
+}
+
+export interface ProductRequest {
+  name: string;
+  description?: string;
+  preparation?: string;
+  ingredients?: string;
+  pricing: ProductPricing;
+  categoryId: string;
+  images?: ProductImage[];
+}
+
+export interface ProductImage {
+  id: string;
+  imageUrl: string;
+  altText: string;
+  displayOrder: number;
+  isPrimary: boolean;
+  product?: Product;
+}
+
+export interface Category {
+  id: string;
+  name: string;
+  description: string;
+  // imageUrl: string; // TODO: Add this property to the API
+  createdAt: string;
+  updatedAt: string;
+}
+
+
+
