c4beee35ecd944a79b22c46ee8bb603d3bd6a172
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-04-05 23:19:44 -0500

    Refactor ProductsPage to enhance layout and styling; replace list with ProductCard components for better presentation

--- src/app/(user)/tienda/page.tsx
+++ src/app/(user)/tienda/page.tsx
@@ -1,19 +1,21 @@
+
+import ProductCard from "@/components/ProductCard";
 import { productService } from "@/services/productService";
+
 
 export default async function ProductsPage() {
   const response = await productService.getAllProducts();
-  const products = response.data;
+  const products = response.data || [];
 
   return (
-    <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.name} - ${product.pricing.unitPrice}
-          </li>
+    <div className="container mx-auto py-8 px-4">
+      <h1 className="text-3xl font-bold mb-8 justify-center">Tienda</h1>
+
+      <div className="flex flex-wrap gap-6">
+        {products?.map((product) => (
+          <ProductCard key={product.id} product={product} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
