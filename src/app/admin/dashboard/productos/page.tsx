a4fdcd89e9a91317e2c53c5c909dc105b3dd0c42
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-04-20 18:01:54 -0500

    Refactor AdminProductPage to use ProductsList component and ProductProvider for improved structure and maintainability

--- src/app/admin/dashboard/productos/page.tsx
+++ src/app/admin/dashboard/productos/page.tsx
@@ -1,28 +1,26 @@
-'use server';
-import ProductCard from "@/components/admin/products/ProductCard";
-import { productService } from "@/services/productService";
-import Link from "next/link";
 
-export default async function AdminProductPage() {
-  const response = await productService.getAllProducts();
-  const products = response.data;
+
+import Link from "next/link";
+import ProductProvider from '@/context/ProductContext';
+import { ProductsList } from "@/components/admin/products/ProductsList";
+
+export default function AdminProductPage() {
+
 
   return (
-    <div className="p-4">
-      <div className="mb-4">
-        <h1 className="text-2xl text-bold">Página de Productos</h1>
+    <ProductProvider >
+      <div className="p-4">
+        <div className="mb-4">
+          <h1 className="text-2xl text-bold">Página de Productos</h1>
+        </div>
+        <ProductsList />
+        <div className="mt-4">
+          <Link href="/admin/dashboard/productos/nuevo">
+            <button className="p-4 bg-green-dark rounded-xl text-whiteygreen font-bold">Adicionar Producto</button>
+          </Link>
+        </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products && products.map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
-      </div>
-      <div className="mt-4">
-        <Link href="/admin/dashboard/productos/nuevo">
-          <button className="p-4 bg-green-dark rounded-xl text-whiteygreen font-bold">Adicionar Producto</button>
-        </Link>
-      </div>
-    </div>
+    </ProductProvider>
   );
 }
 
